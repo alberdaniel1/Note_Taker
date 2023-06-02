f1119df9239783a7468972b785f0a164e9b1a0e0
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-01 20:12:29 -0400

    modified routes into server.js only bcs it wasnt working

--- Develop/server.js
+++ Develop/server.js
@@ -1,36 +1,96 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-
-const PORT = process.env.PORT || 3001;
-
-
 const app = express();
+var PORT = process.env.PORT || 3001;
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'));
-// notes page
-app.get('/notes',(req, res)=>{
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+
+//direct user to correct page depending on
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+app.get("/notes", (req, res) => {
+ res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
-app.get('/api/notes',(req, res)=>{
- res.sendFile(path.join__dirname, '../db/db.json');
+//send json of all notes if user accesses /api/notes
+app.get("/api/notes", (req, res) => {
+fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+    if (error) {
+        return console.log(error)
+    }
+    res.json(JSON.parse(notes))
+  })
 });
 
-app.post('/api/notes',(req, res)=>{
-  console.log(req.body)
-  let db = fs.readFileSync('db/db.json');
-  db = JSON.parse(db);
-  req.json(db);
-})
-
-app.get('*',(req, res)=>{
-  res.sendFile(path.join(__dirname, './public/index.html'));
+//use POST method to bring user input to backend
+app.post("/api/notes", (req, res) => {
+    //declare const for the note currently being saved by user
+    const currentNote = req.body;
+    //retrieve notes from db.json, get id of last note, add 1 to it to create 
+    //new id, save current note with new id
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+      //assign unique id to each new note depending on last id.
+      //if no items in notes array, assign id as 10
+      if (notes.length > 0) {
+      let lastId = notes[notes.length - 1].id
+      var id =  parseInt(lastId)+ 1
+      } else {
+        var id = 10;
+      }
+      //create new note object
+      let newNote = { 
+        title: currentNote.title, 
+        text: currentNote.text, 
+        id: id 
+        }
+      //merge new note with existing notes array
+      var newNotesArr = notes.concat(newNote)
+      //write new array to db.json file and retuern it to user
+      fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(newNotesArr)
+        res.json(newNotesArr);
+      })
+  });
+ 
 });
 
-app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-//read push addto file
+//delete chosen note using delete http method
+app.delete("/api/notes/:id", (req, res) => {
+  let deleteId = JSON.parse(req.params.id);
+  console.log("ID to be deleted: " ,deleteId);
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+    if (error) {
+        return console.log(error)
+    }
+   let notesArray = JSON.parse(notes);
+ 
+
+   
+   for (var i=0; i<notesArray.length; i++){
+     if(deleteId == notesArray[i].id) {
+       notesArray.splice(i,1);
+       fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notesArray), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(notesArray)
+        res.json(notesArray);
+      })
+     }
+  }
+  
+}); 
+});
+//initialize port to start listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
